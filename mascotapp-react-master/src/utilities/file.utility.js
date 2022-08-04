b67f2e4978d07352253a315636fbed5c802d24cd
javiercaliz <javicaliz00@gmail.com>
2022-08-04 12:44:42 -0300

    Ejemplo V2 subido

--- mascotapp-react-master/src/utilities/file.utility.js
+++
@@ -1,8 +0,0 @@
-export const imageToBase64 = (file) => {
-  return new Promise((resolve, reject) => {
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onload = () => resolve(fileReader.result);
-    fileReader.onerror = (error) => reject(error);
-  });
-};
